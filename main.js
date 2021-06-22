7b340b844f5416d9f74495d82c524a5973b5df78
sakshum4 <85113220+sakshum4@users.noreply.github.com>
2021-06-22 15:57:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
+
+
+
+
+
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+
+
+
 function setup()
 {
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+
+
+
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+    poseNet.on('pose', gotPoses);
 }
 
 
@@ -14,12 +40,6 @@ function draw()
 {
 image(video, 0, 0, 600, 500);
 }
-
-
-
-
-
-song = "";
 
 
 
@@ -36,5 +56,25 @@ function preload()
 
 function play()
 {
-    song.play
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+
+
+
+
+function gotPoses(results)
+{
+    if(results.lenght > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.X;
+        leftWristY = results[0].pose.leftWrist.Y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+        rightWristX = result[0].pose.rightWrist.x;
+        rightWristY = result[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    }
 }
