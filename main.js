9a8c0efca21cbbd35c409a2fc1a3eb2fc0b1c3f7
sakshum4 <85113220+sakshum4@users.noreply.github.com>
2021-06-24 17:00:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 
 
 song = "";
+scoreleftWrist = 0;
+scorerightWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
@@ -39,6 +41,46 @@ function modelLoaded()
 function draw()
 {
 image(video, 0, 0, 600, 500);
+fill("#FF0000");
+stroke("#FF0000");
+if(scorerightWrist > 0.2)
+{
+circle(rightWristX,rightWristY,20);
+if(scoreleftWrist > 0.2)
+{
+circle(leftWristX,leftWristY,20);
+if(rightWristY >0 && rightWristY <= 100)
+{
+    document.getElementById("speed").innerhtml = "speed = 0.5x";
+    song.rate(0.5);
+}
+else if(rightWristY >100 && rightWristY <= 200)
+{
+    document.getElementById("speed").innerhtml = "speed = 1x";
+    song.rate(1);
+}
+else if(rightWristY >200 && rightWristY <= 300)
+{
+    document.getElementById("speed").innerhtml = "speed = 1.5x";
+    song.rate(1.5);
+}
+else if(rightWristY >300 && rightWristY <= 400)
+{
+    document.getElementById("speed").innerhtml = "speed = 2x";
+    song.rate(2);
+}
+else if(rightWristY >400 && rightWristY <= 500)
+{
+    document.getElementById("speed").innerhtml = "speed = 2.5x";
+    song.rate(2.5);
+}
+}
+InNumberleftWristY = Number(leftWristY);
+remove_decimals = floor(InNumberleftWristY);
+volume = remove_decimals/500;
+document.getElementById("volume").innerHTML = "volume = " + volume;
+song.setVolume(volume);
+}
 }
 
 
@@ -70,6 +112,10 @@ function gotPoses(results)
     if(results.lenght > 0)
     {
         console.log(results);
+        scorerightWrist = results[0].pose.keypoints[10].score;
+        scoreleftWrist =  results[0].pose.keypoints[9].score;
+        console.log("scorerightWrist = " + scorerightWrist + "scoreleftWrist = " + scoreleftWrist); 
+        console.log("scoreleftWrist = " + scoreleftWrist);
         leftWristX = results[0].pose.leftWrist.X;
         leftWristY = results[0].pose.leftWrist.Y;
         console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
